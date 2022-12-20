8d3242ab76a21288d27bb9c23d3ed8015e023d45
M.Sait TURE <turesait@gmail.com>
2022-12-20 21:41:25 +0100

    setupped chromedriver

--- src/test/java/sait/HelloSait.java
+++ src/test/java/sait/HelloSait.java
@@ -1,4 +1,9 @@
 package sait;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class HelloSait {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+    }
 }
