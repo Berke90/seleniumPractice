50494e72a825f67cd944279adb684aa12f547d04
Berketullah <gafouri2010@gmail.com>
2022-12-20 21:55:08 +0100

    created test 2 java file

---
+++ src/test/java/berke/seleniumTest2/Test2.java
@@ -0,0 +1,15 @@
+package berke.seleniumTest2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+    }
+}
