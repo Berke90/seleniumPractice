4e4d30894fa3e61e4800175f23da63bffdce9931
M.Sait TURE <turesait@gmail.com>
2022-12-20 21:42:12 +0100

    create new chromedriver and get google website

--- src/test/java/sait/HelloSait.java
+++ src/test/java/sait/HelloSait.java
@@ -1,9 +1,14 @@
 package sait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSait {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
     }
 }
